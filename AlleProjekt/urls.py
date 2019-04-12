338c7cdfa3a97979a3f502242bf7bc90e659ac0a
sobelek <Kuba300KS>
2019-04-13 01:21:03 +0200

    base for parsing points

--- AlleProjekt/urls.py
+++ AlleProjekt/urls.py
@@ -23,6 +23,7 @@ from test_model import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name="home"),
-    path("GPS_GET", views.GPS_GET, name="GPS_data")
+    path("GPS_GET", views.GPS_GET, name="GPS_data"),
+    path("test", views.test_points, name="test")
 
 ]
