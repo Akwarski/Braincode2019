ec73caca4b47a1475450eebfc3a50601f0fcf915
sobelek <Kuba300KS>
2019-04-13 00:38:04 +0200

    base vie for getgps

--- test_model/views.py
+++ test_model/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 def home(request):
     return HttpResponse("Hello" + request)
 
 def GPS_GET(request):
     dlugosc = request.GET.get('dlugosc')
     szerokosc = request.GET.get("szerokosc")
-    return HttpResponse("You just got your gps data " + dlugosc + " " + szerokosc)
+    respones_data = {}
+    respones_data['dlugosc'] = dlugosc
+    respones_data['szerokosc'] = szerokosc
+    return HttpResponse(json.dumps(respones_data), content_type="application/json")
+    #return HttpResponse("Your gps data is " + dlugosc + " " + szerokosc)
 # Create your views here.
