408c0c6e78bd0fdc7c7b4b221816ab3d5c4ddaa2
sobelek <Kuba300KS>
2019-04-13 00:18:35 +0200

    base vie for getgps

--- AlleProjekt/urls.py
+++ AlleProjekt/urls.py
@@ -22,6 +22,7 @@ from test_model import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("GPS_GET", views.GPS_GET, name="GPS_data")
 
 ]
--- test_model/views.py
+++ test_model/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse("Hello")
+    return HttpResponse("Hello" + request)
+
+def GPS_GET(request):
+    return HttpResponse("You just got your gps data")
 # Create your views here.
