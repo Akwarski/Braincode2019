b10943bd5e6e56ec4c620b997850c7b68f6bc851
szymani <Szymanski996@gmail.com>
2019-04-13 12:44:23 +0200

    Working open time

--- test_model/distance.py
+++ test_model/distance.py
@@ -10,40 +10,44 @@ from datetime import datetime
 def calc_dist(x1,y1,x2,y2):
     return np.sqrt((x2-x1)^2 + (y2-y1)^2)
 
+
 def calc_dist_to_km(x1,y1,x2,y2):
     return np.sqrt(np.power(((111.320 * np.cos(y2))*x2-(111.320 * np.cos(y1))*x1), 2) + np.power(((110.574)*y2-(110.574)*y1), 2))
 
+
 def check_open(data, current_day, time):
-    format = '%H:%M'
-    opening = datetime.strptime(data["from"], format)
-    if(data["to"] == "24:00"):
-        closing = datetime.strptime("23:59", format)
+    formatT = '%H:%M'
+    opening = datetime.strptime(data["from"], formatT)
+    if data["to"] == "24:00":
+        closing = datetime.strptime("23:59", formatT)
     else:
-        closing = datetime.strptime(data["to"], format)
+        closing = datetime.strptime(data["to"], formatT)
 
-
-    if (datetime.strptime(time, format) - opening).days >= 0 and (closing - datetime.strptime(time, format)).days >= 0:                     #check hour
+    if (datetime.strptime(time, formatT) - opening).days >= 0 and (closing - datetime.strptime(time, formatT)).days >= 0:                     #check hour
         if current_day == data["day"] or (data["day"] == "WORKING" and (current_day != "SUNDAY" or current_day != "SATURDAY" ))\
                 or (data["day"] == "HOLIDAY" and current_day == "SUNDAY"):    #check day
             return True
     return False
 
-def find_closest(dlug, szer,current_day, time, distance = 3):
+
+def find_closest(dlug, szer, current_day, time, distance = 3):
     indexes = []
     paczki = Paczkomat.objects.all()
+
     for paczka in paczki:
+
         data = paczka.dates
         data = data.replace('},{', '}*{')
         data = data[1:-1].split('*')
 
-        if(len(data) <= 1):
-            indexes.append(paczka.id)
-        else:
+        if len(data) > 1:
             for day in data:
                 day = day.replace('""', '"')
                 x = json.loads(day)
-                if check_open(x, current_day, time) == True:
-                    if calc_dist_to_km(dlug, szer, paczka.dlugosc, paczka.szerokosc) <= distance:
-                        indexes.append(paczka.id)
-
+                if calc_dist_to_km(dlug, szer, paczka.dlugosc, paczka.szerokosc) <= distance:
+                    print(paczka.id)
+                    if check_open(x, current_day, time) is True:
+                        indexes.append((paczka.id, True))
+                    else:
+                        indexes.append((paczka.id, False))
     return indexes
