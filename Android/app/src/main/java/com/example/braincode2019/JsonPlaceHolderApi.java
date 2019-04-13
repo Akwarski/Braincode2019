870eb13a56966a41cf83e6d972cd1586000276ce
Akwarski <akwarski@gmail.com>
2019-04-13 12:30:26 +0200

    all delivers on map

--- Android/app/src/main/java/com/example/braincode2019/JsonPlaceHolderApi.java
+++ Android/app/src/main/java/com/example/braincode2019/JsonPlaceHolderApi.java
@@ -6,10 +6,13 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
+//http://192.168.43.28:8000/GPS_GET?dlugosc=20&szerokosc=50&dzien=MONDAY&godzina=23:04&odleglosc=0
 public interface JsonPlaceHolderApi {
     @GET("GPS_GET")
     Call<List<JsonData>> getPost(@Query("dlugosc") int dlugosc,
-                                 @Query("szerokosc") int szerokosc);
+                                 @Query("szerokosc") int szerokosc,
+                                 @Query("dzien") String dzien,
+                                 @Query("godzina") String godzina);
 
     //http://localhost:8000/GPS_GET?dlugosc=100&szerokosc=200
     //http://192.168.43.28:8000/test?1=3&2=5
