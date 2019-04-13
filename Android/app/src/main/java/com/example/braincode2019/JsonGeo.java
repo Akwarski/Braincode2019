9e630a9e0fedd77d548c56a890f5893fea25753a
Akwarski <akwarski@gmail.com>
2019-04-13 02:06:57 +0200

    almost json

---
+++ Android/app/src/main/java/com/example/braincode2019/JsonGeo.java
@@ -0,0 +1,14 @@
+package com.example.braincode2019;
+
+public class JsonGeo {
+    private double dlugosc;
+    private double szerokosc;
+
+    public double getDlugosc() {
+        return dlugosc;
+    }
+
+    public double getSzerokosc() {
+        return szerokosc;
+    }
+}
